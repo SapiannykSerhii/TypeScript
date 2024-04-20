5db52704bcae1eb689245cbfb2dab519f850b638
Serhii Sapiannyk <serhii09@icloud.com>
2024-04-20 16:20:29 +0200

    add rate product & fix App

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ import { products } from './data/products';
 function App() {
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product product={products[0]} />
-      <Product product={products[1]} />
-      <Product product={products[2]} />
-      <Product product={products[3]} />
-      <Product product={products[4]} />
+      {products.map(product => (
+          <Product key={product.id} product={product} />
+        )
+      )}
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -27,7 +27,8 @@ export function Product({product}: ProductProps) {
       
 
       {details && <div>
-        <p className="font-bold">{product.description}</p>
+        <p>{product.description}</p>
+        <p>Rate: <span style={{ fontWeight: 'bold',}}>{product.rating.rate}</span></p>
       </div>}
       
     </div>
