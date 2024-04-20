59d3eb47a7a2899b9ca533ac59b55e4c1fde33d9
Serhii Sapiannyk <serhii09@icloud.com>
2024-04-20 16:09:00 +0200

    added products and functionality on a button

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
       <Product product={products[0]} />
+      <Product product={products[1]} />
+      <Product product={products[2]} />
+      <Product product={products[3]} />
+      <Product product={products[4]} />
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import { IProduct } from "../models";
 
 interface ProductProps {
   product: IProduct
 }
 
-export function Product(props: ProductProps) {
+export function Product({product}: ProductProps) {
+  const [details, setDetails] = useState(false)
   
+  const btnBgClassName = details ? 'bg-yellow-300' : 'bg-blue-300'
+  const btnClasses = ['py-2 px-4 border', btnBgClassName]
   
   return (
     <div
       className="border px-2 px-4 flex flex-col items-center mb-2"
     >
-      Product!
+      <img src={product.image} className="w-1/6" alt={ product.title} />
+      <p>{product.title}</p>
+
+      <button
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails(prev => !prev)}
+      >
+        {details ? 'Hide details' : 'Show details'}
+      </button>
+      
+
+      {details && <div>
+        <p className="font-bold">{product.description}</p>
+      </div>}
+      
     </div>
   )
 }
